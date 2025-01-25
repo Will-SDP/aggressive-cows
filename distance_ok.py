9502c63b974ae3f83497bcc68c773f497ec9aaf7
Will-SDP <William.e.cooper@student.shu.ac.uk>
2025-01-25 10:17:47 +0000

    Added logic to break out early if enough cows have been placed to satisfy the condition

--- distance_ok.py
+++ distance_ok.py
@@ -7,6 +7,11 @@ def distance_ok(stalls, cows, distance):
         if stalls[i] - prev >= distance:
             prev = stalls[i]
             cows_placed += 1 
+
+            # If we have already placed enough cows there is no need to
+            # continue the evaluation so return True
+            if cows_placed >= cows:
+                return True
     return cows_placed >= cows 
 
 
